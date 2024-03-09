48a288e03e0777a5850122aae692429f57afb33d
Tom Britton <britton.tm@gmail.com>
2024-03-09 11:10:12 -0600

    Added endpoint for server-side generation of qr codes.

---
+++ src/helpers/string.ts
@@ -0,0 +1,7 @@
+export function makeUrlSafe(unsafeString: string): string {
+  // Regular expression to match all characters except URL safe ones
+  const urlSafeRegex = /[^a-zA-Z0-9\-._#?&@:/]/g;
+
+  // Replace non-URL safe characters with an empty string
+  return unsafeString.replace(urlSafeRegex, "");
+}
---
+++ src/pages/qr.svg.ts
@@ -0,0 +1,35 @@
+import QRCode from 'qrcode';
+import { makeUrlSafe } from '../helpers/string';
+
+export async function GET({ request }: {
+  request: Request;
+}) {
+  const url = new URL(request.url);
+  const partyId = url.searchParams.get('partyid');
+
+  if (!partyId) {
+    return new Response(null, { status: 404 });
+  }
+
+  const svgUrl = `${url.protocol}//${url.hostname}${url.hostname === 'localhost' ? `:${url.port}` : ''}/join/${makeUrlSafe(partyId)}`
+  console.log(svgUrl)
+  try {
+    const QRdata = await QRCode.toString(svgUrl, {
+      errorCorrectionLevel: 'H',
+      type: 'svg',
+    });
+
+    return new Response(QRdata, {
+      status: 200,
+      headers: {
+        'Content-Type': 'image/svg+xml',
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response(null, {
+      status: 400,
+      statusText: 'Error generating QR Code',
+    });
+  }
+}
