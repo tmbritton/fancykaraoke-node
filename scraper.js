38cd51ba126c674a62b19c7eaa91a8c829a51ed2
Tom Britton <britton.tm@gmail.com>
2024-04-21 15:03:45 -0500

    Automate song import

---
+++ scraper.js
@@ -0,0 +1,91 @@
+import { load } from "cheerio";
+import { createClient } from "@libsql/client";
+import axios from 'axios';
+
+const client = createClient({
+  url: process.env.FK_DB_URL,
+  authToken: process.env.FK_DB_TOKEN
+});
+
+const args = process.argv.slice(2);
+const timeStamp = Date.now();
+const direction = 'desc';
+
+const getCount = async () => {
+  const result = await client.execute({
+    sql: 'SELECT count(*) from songs',
+    args: []
+  });
+  return result?.rows?.[0]?.['count (*)'];
+}
+
+const getKaraokeData = async (start, count) => {
+  const url = `https://www.karaokenerds.com/Community/BrowseJson/?length=${count}&start=${start}&order[0][column]=3&order[0][dir]=${direction}&_=${timeStamp}`;
+  const songData = await axios.get(url).then(({ data }) => {
+    const songs = data?.data
+    const songList = []
+
+    const isYoutubeIdInList = (id) => songList.findIndex(song => song.youtubeId === id) > -1
+
+    songs.forEach(item => {
+      const artist = load(item[1])('a').text();
+      const title = load(item[2])('a').text();
+      const date = item[3];
+      const uploader = load(item[4])('a').text();
+      const youtubeLink = item[5].split('|')[0];
+      const videoIdMatch = youtubeLink.match(/(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([^"&?\/\s]{11})/);
+      const youtubeId = videoIdMatch ? videoIdMatch[1] : null;
+  
+      if (youtubeId && artist && title && !isYoutubeIdInList(youtubeId)) {
+        songList.push({
+          artist,
+          title,
+          date,
+          uploader,
+          youtubeId
+        })
+      }
+    });
+    return songList;
+  });
+  return songData;
+};
+
+const main = async () => {
+  const count = args[0];
+  const start = await getCount() + 1;
+  const songList = await getKaraokeData(start, count);
+  
+  console.log(`Songs loaded: ${songList.length}`);
+
+  let success = 0;
+  let error = 0;
+
+  for (const song of songList) {
+    try {
+      const result = await client.execute({
+        sql: "insert into songs (artist, title, uploader, youtube_id) values (:artist, :title, :uploader, :youtube_id)",
+        args: {
+          artist: song.artist,
+          title: song.title,
+          uploader: song.uploader,
+          youtube_id: song.youtubeId
+        }
+      });
+
+      if (result) {
+        success++;
+      }
+    } catch (e) {
+      error++;
+    }
+  }
+
+  console.log(`Success: ${success}`);
+  console.log(`Error: ${error}`);
+  return;
+};
+
+main()
+
+export { getKaraokeData }
